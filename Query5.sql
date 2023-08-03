c85adcc453aad0d5554742f0e5427d7861f31e37
Ekpisuena <ekaye21@gmail.com>
2023-08-03 12:40:57 -0400

    Create Query5.sql
    Total revenue per store

---
+++ Query5.sql
@@ -0,0 +1,6 @@
+--total revenue per store
+SELECT s.store_id, sum(p.amount) as total_revenue
+from payment p
+JOIN staff st ON p.staff_id=st.staff_id
+JOIN store s ON st.store_id=s.store_id
+GROUP BY s.store_id;
